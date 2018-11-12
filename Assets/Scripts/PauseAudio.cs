4b3162603fa909a479214df97b2dd5d83bc4e246
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-12 19:59:01 +0100

    Meti pausa presionando P

---
+++ Assets/Scripts/PauseAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAudio : MonoBehaviour {
+    AudioSource m_MyAudioSource;
+    [SerializeField] private GameObject pausePanel;
+    // Use this for initialization
+    void Start () {
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("p"))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+
+                m_MyAudioSource.Pause();
+            }
+            else
+            {
+
+                m_MyAudioSource.Play();
+            }
+        }
+    }
+}
