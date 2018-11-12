bf19fd191a7b12737eeb9057021cac5bc08b9bbf
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-12 21:08:46 +0100

    Cree un bosquecito

---
+++ Assets/TerrainConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+class TerrainConfig : MonoBehaviour
+{
+    public int TreeRenderDist = 10000;
+    public Terrain terrain;
+    void Start()
+    {
+
+        terrain.treeDistance = TreeRenderDist;
+        Debug.Log(terrain.treeDistance);
+    }
+}
