2e89f4c8d1b3364e161cc777b5f5bfbde3c3e097
lets.create.a.video.game <lets.create.a.video.game@gmail.com>
2018-11-13 19:18:27 +0100

    Update Assets/Scripts/Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Entrar (puerta1 puerta)
+    {
+        SceneManager.LoadScene(puerta.getEscena());
+    }
+}
