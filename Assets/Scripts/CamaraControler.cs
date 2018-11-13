606fedb469696a40b4ed88eb2a48b4fe08dc609c
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-13 20:48:40 +0100

    Camara arreglada final

--- Assets/Scripts/CamaraControler.cs
+++ Assets/Scripts/CamaraControler.cs
@@ -28,22 +28,41 @@ public class CamaraControler : MonoBehaviour
         Debug.Log(angleBetween);
         Debug.Log(MaxCamaraRotationDown + 31f);
         Debug.Log(MaxCamaraRotationUp + 91f);
-        if (angleBetween < 150f && angleBetween > 60f)
+        if (angleBetween < 160f && angleBetween > 70f)
         {
             float pitch = Input.GetAxisRaw("Mouse Y") * -1f * sensitivity * Time.deltaTime;
             transform.Rotate(pitch * Vector3.right, Space.Self);
         }
+        if (angleBetween < 170f && angleBetween >= 160f)
+        {
+            float pitch1 = Input.GetAxisRaw("Mouse Y") * -1f * sensitivity * Time.deltaTime;
+            Debug.Log(pitch1);
+            if (pitch1 > 0)
+            {
+                Debug.Log("Correction not working");
+                transform.Rotate(pitch1 * Vector3.right, Space.Self);
+            }
+        }
+        if (angleBetween > 61f && angleBetween <= 70f)
+        {
+            float pitch2 = Input.GetAxisRaw("Mouse Y") * -1f * sensitivity * Time.deltaTime;
+            if (pitch2 < 0)
+            {
+                Debug.Log("correction not working");
+                transform.Rotate(pitch2 * Vector3.right, Space.Self);
+            }
+        }
         if (angleBetween < MaxCamaraRotationDown+30f)
         {
             Debug.Log("ActiveMenorque60");
-            float pitch = -60f * Time.deltaTime;
             transform.localRotation = Quaternion.Euler(60, 0, 0);
+            
         }
-        if (angleBetween > MaxCamaraRotationUp+90f)
+        if (angleBetween > MaxCamaraRotationUp+110f)
         {
             Debug.Log("Activemayorque150");
-            float pitch = 60f * Time.deltaTime;
-            transform.localRotation = Quaternion.Euler(-28, 0, 0);
+             transform.localRotation = Quaternion.Euler(-28, 0, 0);
+
         }
 
 
