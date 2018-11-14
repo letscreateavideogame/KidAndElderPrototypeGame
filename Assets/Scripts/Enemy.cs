838c3408e79a3f3a012c1cee89e56119d078c551
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-14 18:18:49 +0100

    Cambios, muchos cambios relacionado al script de los movimeintos y interacciones

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Interactable {
+
+	// Use this for initialization
+public override void Interact()
+    {
+        base.Interact();
+        //Attack it
+    }
+}
