31dfdf0d26507e021ee249efed994b9545eda5bf
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-14 17:48:49 +0100

    MEjorado el script

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnimyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
 
     public float lookRadius = 10f;
 
@@ -23,6 +23,19 @@ public class EnimyController : MonoBehaviour {
         if(distance <= lookRadius)
         {
             agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //Attack the target
+                FaceTarget();
+            }
         }
 	}
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
 }
