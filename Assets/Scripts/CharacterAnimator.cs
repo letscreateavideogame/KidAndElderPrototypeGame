d1b83c82f4bdeede60985e6936926f22f62777fe
letscreateavideogame <lets.create.a.video.game@gmail.com>
2018-11-16 10:30:46 +0100

    Actualizada la script,(Contiene errores, al llegar a casa los arreglo)

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour {
+    const float locomationAnimationSmoothTime = .1f;
+
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+        animator = GetComponent<Animator>();
+        agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float speedPercentage = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
 	}
 }
